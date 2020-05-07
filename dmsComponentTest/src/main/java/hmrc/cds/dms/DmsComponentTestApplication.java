959a6b6cab2974d09fedce6c9bbae47507e78880
Suresh Babu G <babugsuresh@users.noreply.github.com>
2020-05-07 23:22:47 +0100

    new changes

--- dmsComponentTest/src/main/java/hmrc/cds/dms/DmsComponentTestApplication.java
+++ dmsComponentTest/src/main/java/hmrc/cds/dms/DmsComponentTestApplication.java
@@ -1,11 +1,13 @@
 package hmrc.cds.dms;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -31,6 +33,7 @@ import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 
@@ -50,7 +53,7 @@ import com.ibm.wsdl.PortTypeImpl;
 public class DmsComponentTestApplication implements CommandLineRunner {
 
 	private static final String BASEDIR = null;
-	//private static Object //TODO;
+	// private static Object //TODO;
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
@@ -59,7 +62,6 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 		app.run();
 	}
 
-	@SuppressWarnings("deprecation")
 	public void run(String... args) throws UnsupportedOperationException, SOAPException, IOException, Exception {
 
 		logger.debug("This is a debug message");
@@ -68,14 +70,9 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 		logger.error("This is an error message");
 
 		System.out.println("\n----JESUS is my GOD----");
-		
-		//ReportBean bean = new ReportBean();
-		
-		//bean.getEnvironments();
 
 		Yaml yaml = new Yaml();
-		Reader yamlFile = new FileReader(
-				"D:\\Users\\U.7898083-1\\git\\dmsComponentTest\\dmsComponentTest\\src\\main\\resources\\application.yml");
+		Reader yamlFile = new FileReader("src/main/resources/application.yml");
 
 		@SuppressWarnings("unchecked")
 		Map<String, Object> yamlMaps = (Map<String, Object>) yaml.load(yamlFile);
@@ -102,6 +99,10 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 		String soapenvelope = null;
 		String systemtocall = null;
 		boolean loopBack = false;
+
+		List<OperationName> operationNames = new ArrayList<OperationName>();
+		List<ServiceName> serviceNames = new ArrayList<ServiceName>();
+		List<ReportBean> rb = new ArrayList<ReportBean>();
 
 		for (String env : envs) {
 
@@ -154,33 +155,19 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 								System.out.println(
 										"Total Number of operations in " + wsdlName + " : " + operationList.size());
 
+								// List<OperationName> operationNames = new ArrayList<OperationName>();
+
 								for (Operation opname : operationList) {
+									//System.out.println("List of Operations: " +opname.getName() );
+									//System.out.println("---Started Looking for Request XMLs---");
 
 									soapenvelope = getRequestEnvelopeToString(opname.getName());
-									/*
-									 * System.out.println("\nOperationName: " + opname.getName() +
-									 * "\nis there a Request XML exists?: " +
-									 * !soapenvelope.equalsIgnoreCase("empty"));
-									 */
-
-									File htmlTemplateFile = new File("src/main/resources/Report.html");
-									System.out.println("testtt: " + htmlTemplateFile.getName());
-
-									String htmlString = FileUtils.readFileToString(htmlTemplateFile, "UTF-8");
-									String title = "New Page";
-									
-									String body = "<div class=\"container\"><table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th><th>Column 4</th><th>Column 5</th></tr></thead><tbody><tr><td>Cell 1</td><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td><td>Cell 5</td></tr><tr><td>Cell 1</td><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td><td>Cell 5</td></tr><tr><td>Cell 1</td><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td><td>Cell 5</td></tr><tr><td>Cell 1</td><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td><td>Cell 5</td></tr><tr><td>Cell 1</td><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td><td>Cell 5</td></tr></tbody></table></div>";
-									//String body = soapenvelope;
-									htmlString = htmlString.replace("$title", title);
-									htmlString = htmlString.replace("$body", body);
-									File newHtmlFile = new File("src/main/resources/new.html");
-									FileUtils.writeStringToFile(newHtmlFile, htmlString, "UTF-8");
-									System.out.println(
-											"Got Bodyyyy: " + FileUtils.readFileToString(newHtmlFile, "UTF-8"));
 
 									if (!soapenvelope.equalsIgnoreCase("empty")) {
+
+										System.out.println("Request XML available for Operation: " + opname.getName());
 										System.setProperty("java.net.useSystemProxies", "true");
-										System.out.println("SOAPenvelopetoString: " + soapenvelope);
+										System.out.println("SOAP Request Payload: " + soapenvelope);
 
 										System.out.println("SOAP endpoint to Call: " + endpoint);
 
@@ -194,15 +181,55 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 										SOAPMessage soapResponse = soapConnection.call(request, endpoint);
 
 										// System.out.println(printSOAPResponse(soapResponse));
+										// ByteArrayOutputStream streamOut = new ByteArrayOutputStream();
 
-										TransformerFactory transformerFactory = TransformerFactory.newInstance();
-										Transformer transformer = transformerFactory.newTransformer();
-										Source sourceContent = soapResponse.getSOAPPart().getContent();
-										System.out.print("\nResponse SOAP Message = ");
-										System.out.print("\n");
-										StreamResult result = new StreamResult(System.out);
-										transformer.transform(sourceContent, result);
+										StringWriter sw = new StringWriter();
 
+										try {
+											TransformerFactory transformerFactory = TransformerFactory.newInstance();
+											Transformer transformer = transformerFactory.newTransformer();
+											Source sourceContent = soapResponse.getSOAPPart().getContent();
+
+											System.out.print("\nResponse SOAP Message = ");
+											System.out.print("\n");
+											// StreamResult result = new StreamResult(System.out);
+											StreamResult result = new StreamResult(sw);
+											transformer.transform(sourceContent, result);
+
+										} catch (TransformerException e) {
+											throw new RuntimeException(e);
+										}
+
+										OperationName operationName = new OperationName();
+										operationName.setOperationName(opname.getName());
+
+										if (!soapResponse.getSOAPBody().hasFault()) {
+											operationName.setStatus("Running");
+										} else {
+											operationName.setStatus("  Down ");
+										}
+										operationName.setResponse(sw.toString());
+
+										operationNames.add(operationName);
+										System.out.println("operationName Added successfully"+operationName);
+
+										// System.out.println("\noperationNames: " + operationNames);
+
+										ServiceName services = new ServiceName();
+										services.setSystemName(systemtocall);
+										services.setServiceName(wsdlName);
+										services.setOperationNames(operationNames);
+										serviceNames.add(services);
+										System.out.println("serviceName Added successfully"+services);
+
+										ReportBean reportBean = new ReportBean();
+										reportBean.setEnvName(env);
+										reportBean.setServiceNames(serviceNames);
+										rb.add(reportBean);
+										System.out.println("ReportBean Added successfully"+reportBean);
+
+									}else {
+										System.out.println("No Request XML is provided for: "+opname.getName());
 									}
 
 								}
@@ -220,6 +247,105 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 			}
 
 		}
+		System.out.println("LOGGGGGGGGGGGGGGG");
+		System.out.println("operationNames: " + operationNames);
+		System.out.println("serviceNames: " + serviceNames);
+		System.out.println("ReportBean: " + rb);
+
+		generateHTMLfile(rb);
+
+	}
+
+	private void generateHTMLfile(List<ReportBean> rb) throws IOException {
+		
+		System.out.println("JESUS is MY LORD");
+
+		File htmlTemplateFile = new File("src/main/resources/Report2.html");
+
+		String htmlString = FileUtils.readFileToString(htmlTemplateFile, "UTF-8");
+
+		String title = "New Page";
+		
+		String body = "";
+
+		String tag1 = "<section>  <h1>";
+		String tag2 = "</h1><div class=\"tbl-header\">\r\n"
+				+ "    <table cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\r\n" + "      <thead>\r\n"
+				+ "        <tr><b>\r\n" + "          <th><b>System Name<b/></th>\r\n"
+				+ "          <th><b>Service Name<b/></th>\r\n" + "          <th><b>Operation Name<b/></th>\r\n"
+				+ "          <th><b>Status<b/></th>\r\n" + "          <th><b>Response XML<b/></th>\r\n"
+				+ "        </tr>\r\n" + "      </thead>\r\n" + "    </table>\r\n" + "  </div>\r\n"
+				+ "  <div class=\"tbl-content\">\r\n"
+				+ "    <table cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\r\n" + "      <tbody>";
+
+		String tag3 = "</tbody>\r\n" + "    </table>\r\n" + "  </div>\r\n" + "</section>";
+
+		String tag4 = "<tr><td>";
+		String tag5 = "</td></tr>";
+		String tag6 = "</td><td>";
+		String green = "<span class=\"label success\">";
+		String red = "<span class=\"label danger\">";
+		String blue = "<span class=\"label label-info\">";
+		String tag7 = "</span>";
+
+		String tag8 = "<button type=\"button\" class=\"collapsible\">";
+		String tag9 = "</button>";
+		String tag10 = "<div class=\"content\">";
+		String tag11 = "</div>";
+
+		// To Make response button to be clickable
+		String js = "<script>\r\n" + "var coll = document.getElementsByClassName(\"collapsible\");\r\n" + "var i;\r\n"
+				+ "\r\n" + "for (i = 0; i < coll.length; i++) {\r\n"
+				+ "  coll[i].addEventListener(\"click\", function() {\r\n"
+				+ "    this.classList.toggle(\"active\");\r\n" + "    var content = this.nextElementSibling;\r\n"
+				+ "    if (content.style.display === \"block\") {\r\n" + "      content.style.display = \"none\";\r\n"
+				+ "    } else {\r\n" + "      content.style.display = \"block\";\r\n" + "    }\r\n" + "  });\r\n"
+				+ "}\r\n" + "</script>";
+
+		// <button type="button" class="collapsible">Response</button>
+
+		// tag8+Response+tage9
+
+		// tag8+op.getResponse()+tag9
+
+		
+
+		StringBuilder builder = new StringBuilder();
+
+		for (ReportBean r : rb) {
+			String toPublish = "";
+			String dataToLoad = "";
+			StringBuilder localBuilder = new StringBuilder();
+			for (ServiceName sn : r.getServiceNames()) {
+				for (OperationName op : sn.getOperationNames()) {
+					String sname = sn.getServiceName().substring(0, sn.getServiceName().lastIndexOf(".")).replaceAll("\\SOAP.*?\\b", "");
+					String color = null;
+					if(op.getStatus().equalsIgnoreCase("Running")) {
+						color = green + op.getStatus() + tag7;
+					}else {
+						color = red + op.getStatus() + tag7;
+					}
+					//String cc = "<div class=\"content\">"+op.getResponse()+"</div>";
+					String cc = "<div class=\"content\">Testing</div>";
+					dataToLoad = tag4 + sn.getSystemName() + tag6 + sname + tag6 + op.getOperationName()
+							+ tag6 + color + tag6 + tag8 + "Response" + "</button>" + cc + tag5;
+					localBuilder.append(dataToLoad);
+				}
+			}
+
+			toPublish = localBuilder.toString();
+			toPublish = tag1 + r.getEnvName() + tag2 + toPublish + tag3;
+			builder.append(toPublish);
+		}
+
+		String finalDataToPublish = builder.toString();
+
+		body = finalDataToPublish + js;
+
+		htmlString = htmlString.replace("$title", title);
+		htmlString = htmlString.replace("$body", body);
+		File newHtmlFile = new File("src/main/resources/new.html");
+		FileUtils.writeStringToFile(newHtmlFile, htmlString, "UTF-8");
 
 	}
 
@@ -309,11 +435,5 @@ public class DmsComponentTestApplication implements CommandLineRunner {
 		}
 		return contentBuilder.toString();
 	}
-	
-	private static void generateHTMLfile () {
-		//TODO
-	}
-	
-	
 
 }
--- dmsComponentTest/src/main/java/hmrc/cds/dms/ReportBean.java
+++ dmsComponentTest/src/main/java/hmrc/cds/dms/ReportBean.java
@@ -3,73 +3,103 @@ package hmrc.cds.dms;
 import java.util.List;
 
 public class ReportBean {
+	public String envName;
+	public List<ServiceName> serviceNames = null;
 
-	private List<Environments> environments;
-
-	public List<Environments> getEnvironments() {
-		return environments;
+	public String getEnvName() {
+		return envName;
 	}
 
-	public void setEnvironments(List<Environments> environments) {
-		this.environments = environments;
+	public void setEnvName(String envName) {
+		this.envName = envName;
 	}
 
-}
-
-class Environments {
-	private List<ServiceNames> serviceNames;
-
-	public List<ServiceNames> getServiceNames() {
+	public List<ServiceName> getServiceNames() {
 		return serviceNames;
 	}
 
-	public void setServiceNames(List<ServiceNames> serviceNames) {
+	public void setServiceNames(List<ServiceName> serviceNames) {
 		this.serviceNames = serviceNames;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "ReportBean [envName=" + envName + ", serviceNames=" + serviceNames + "]";
+	}
 }
 
-class ServiceNames {
-	private List<OperationNames> operationNames;
+class ServiceName {
 
-	public List<OperationNames> getOperationNames() {
+	public String systemName;
+	public String serviceName;
+
+	public String getSystemName() {
+		return systemName;
+	}
+
+	public void setSystemName(String systemName) {
+		this.systemName = systemName;
+	}
+
+	public List<OperationName> operationNames = null;
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public List<OperationName> getOperationNames() {
 		return operationNames;
 	}
 
-	public void setOperationNames(List<OperationNames> operationNames) {
+	public void setOperationNames(List<OperationName> operationNames) {
 		this.operationNames = operationNames;
 	}
-}
 
-class OperationNames {
-	private String name;
-
-	private String status;
-
-	private String response;
-
-	public void setName(String name) {
-		this.name = name;
+	@Override
+	public String toString() {
+		return "ServiceName [systemName=" + systemName + ", serviceName=" + serviceName + ", operationNames="
+				+ operationNames + "]";
 	}
 
-	public String getName() {
-		return this.name;
+}
+
+class OperationName {
+
+	public String operationName;
+	public String status;
+	public String response;
+
+	public String getOperationName() {
+		return operationName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setOperationName(String operationName) {
+		this.operationName = operationName;
 	}
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
-	public String getStatus() {
-		return this.status;
-	}
-
 	public void setResponse(String response) {
 		this.response = response;
 	}
 
-	public String getResponse() {
-		return this.response;
+	@Override
+	public String toString() {
+		return "OperationName [operationName=" + operationName + ", status=" + status + ", response=" + response + "]";
 	}
+
 }
